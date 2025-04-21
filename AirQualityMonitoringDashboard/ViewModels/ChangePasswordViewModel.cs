deb63d5367cb523f5d8134ef4a563be3320d4158
shenonk <sakurukulasuriya@students.nsbm.ac.lk>
2025-04-21 12:48:39 +0530

    Added Regex based Validations

--- AirQualityMonitoringDashboard/ViewModels/ChangePasswordViewModel.cs
+++ AirQualityMonitoringDashboard/ViewModels/ChangePasswordViewModel.cs
@@ -5,13 +5,16 @@ namespace AirQualityMonitoringDashboard.ViewModels
     public class ChangePasswordViewModel
     {
         [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
         [Display(Name = "New Password")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,40}$",
+            ErrorMessage = "Password must be 8–40 characters long and contain at least one letter," +
+            " one number, and one special character.")]
+        
         [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
         public string NewPassword { get; set; }
 
